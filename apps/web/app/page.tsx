e6af7cd9af9262104e50749ca96d818e2c7e6463
RobeSantoro <santoro.robe@gmail.com>
2023-11-08 01:08:45 +0100

    Add import and save json

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,11 +4,11 @@ import Editor from "@/ui/editor";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(7vh)]">
       <a
         href="https://github.com/steven-tey/novel"
         target="_blank"
-        className="absolute bottom-5 left-5 z-10 max-h-fit rounded-lg p-2 transition-colors duration-200 hover:bg-stone-100 sm:bottom-auto sm:top-5"
+        className="absolute z-10 p-2 transition-colors duration-200 rounded-lg bottom-5 left-5 max-h-fit hover:bg-stone-100 sm:bottom-auto sm:top-5"
       >
         <Github />
       </a>
